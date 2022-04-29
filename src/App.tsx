aac26aabc43999091ea8a39a165ad2325533e46c
Ian Obermiller <ian@obermillers.com>
2022-04-29 11:27:01 -0400

    Use hash routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import Router from "preact-router";
+import Router, { CustomHistory } from "preact-router";
 import { CalendarList } from "./CalendarList";
 import { Calendar } from "./Calendar";
 import styles from "./App.module.css";
+import { createHashHistory } from "history";
 
 export function App() {
   return (
     <div class={styles.app}>
-      <Router>
+      <Router history={createHashHistory() as unknown as CustomHistory}>
         <CalendarList path="/" />
         <Calendar path="/:id" />
       </Router>
