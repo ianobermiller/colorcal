3aaa7859ebbe2f21c9ab84f30a6ea0c8bad3a8a1
Ian Obermiller <ian@obermillers.com>
2022-05-02 22:16:02 -0400

    Add calendar cards to the calendar list

--- src/CalendarList.tsx
+++ src/CalendarList.tsx
@@ -1,8 +1,9 @@
 import { useCurrentUser, useQuery } from "thin-backend/react";
-import { createRecord, query } from "thin-backend";
-import { useRef } from "preact/hooks";
+import { Calendar, createRecord, logout, query } from "thin-backend";
+import { useCallback, useRef } from "preact/hooks";
 import { toISODateString } from "./dateUtils";
 import { Button } from "./Button";
+import styles from "./CalendarList.module.css";
 
 interface Props {
   path: string;
@@ -12,35 +13,60 @@ export function CalendarList({}: Props) {
   const user = useCurrentUser();
   const calendars = useQuery(query("calendars").orderByDesc("updatedAt"));
   const calendarName = useRef<HTMLInputElement>(null);
+
+  const createCalendar = useCallback(() => {
+    const startDate = new Date();
+    const endDate = new Date(startDate);
+    endDate.setDate(endDate.getDate() + 7);
+
+    createRecord("calendars", {
+      endDate: toISODateString(endDate),
+      startDate: toISODateString(startDate),
+      title: calendarName.current?.value,
+    });
+  }, []);
+
   return (
     <>
-      <p>Hello {user?.email}!</p>
-      <h3>Calendars</h3>
-      <ul>
+      <h1>Calendars</h1>
+      <p>
+        Logged in as {user?.email}{" "}
+        <a href="javascript:;" onClick={() => logout()}>
+          Logout
+        </a>
+      </p>
+      <ul class={styles.list}>
         {calendars?.map((cal) => (
-          <li>
-            <a href={`/${cal.id}`}>{cal.title}</a>
+          <li class={styles.calendar}>
+            <a href={`/${cal.id}`}>
+              <h2>{cal.title}</h2>
+              <p>
+                {formatDate(cal.startDate)} to {formatDate(cal.endDate)}
+              </p>
+            </a>
           </li>
         ))}
       </ul>
-      <div>
-        <input type="text" ref={calendarName} />
-        <Button
-          onClick={() => {
-            const startDate = new Date();
-            const endDate = new Date(startDate);
-            endDate.setDate(endDate.getDate() + 7);
-
-            createRecord("calendars", {
-              endDate: toISODateString(endDate),
-              startDate: toISODateString(startDate),
-              title: calendarName.current?.value,
-            });
+      <div class={styles.addCalendar}>
+        <input
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              createCalendar();
+              e.currentTarget.value = "";
+            }
           }}
-        >
-          Add
-        </Button>
+          ref={calendarName}
+          type="text"
+        />
+        <Button onClick={createCalendar}>Add</Button>
       </div>
     </>
   );
+}
+
+function formatDate(dateString: string): string {
+  return new Date(dateString).toLocaleDateString(undefined, {
+    dateStyle: "medium",
+    timeZone: "UTC",
+  });
 }
