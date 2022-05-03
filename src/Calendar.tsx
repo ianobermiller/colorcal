46df133782edb2d3f3e6575b9245de7db38a4e72
Ian Obermiller <ian@obermillers.com>
2022-05-02 21:58:33 -0400

    Sort categories by first appearance in the calendar

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -17,20 +17,31 @@ export function Calendar({ id }: Props) {
   const calendar = useQuerySingleResult(
     query("calendars").where("id", id || "")
   );
-  const days = useQuery(query("days").where("calendarId", calendar?.id!));
-  const categories =
-    useQuery(query("categories").where("calendarId", calendar?.id!)) || [];
+  const days = useQuery(
+    query("days").where("calendarId", calendar?.id!).orderByAsc("date")
+  );
+  const categories = useQuery(
+    query("categories")
+      .where("calendarId", calendar?.id!)
+      .orderByAsc("createdAt")
+  );
 
-  if (!id || !calendar) {
-    return null;
+  if (!id || !calendar || !days || !categories) {
+    return <h1>Loading...</h1>;
   }
 
-  const dayByDate = (days || []).reduce((acc, day) => {
+  const sortedCategories = sortBy(
+    categories,
+    (cat) => lastIfNotFound(days.findIndex((d) => d.categoryId === cat.id)),
+    (cat) => lastIfNotFound(days.findIndex((d) => d.halfCategoryId === cat.id))
+  );
+
+  const dayByDate = days.reduce((acc, day) => {
     acc[day.date] = day;
     return acc;
   }, {} as Record<string, Day | undefined>);
 
-  const countByCategory = (days || []).reduce((acc, day) => {
+  const countByCategory = days.reduce((acc, day) => {
     if (day.categoryId) {
       const existing = acc[day.categoryId] || 0;
       acc[day.categoryId] = existing + 1;
@@ -87,7 +98,7 @@ export function Calendar({ id }: Props) {
                 calendarId={calendar.id}
                 day={dayByDate[toISODateString(date)]}
                 date={date}
-                categories={categories}
+                categories={sortedCategories}
                 startDate={calendar.startDate}
               />
             ) : (
@@ -99,9 +110,34 @@ export function Calendar({ id }: Props) {
 
       <CategoryList
         calendarId={calendar.id}
-        categories={categories}
+        categories={sortedCategories}
         countByCategory={countByCategory}
       />
     </div>
   );
+}
+
+function sortBy<T>(
+  array: T[],
+  ...predicates: Array<(element: T) => string | number>
+): T[] {
+  return array.slice().sort((a, b) => {
+    if (a === b) {
+      return 0;
+    }
+
+    for (const predicate of predicates) {
+      const aValue = predicate(a);
+      const bValue = predicate(b);
+      if (aValue === bValue) {
+        continue;
+      }
+      return aValue > bValue ? 1 : -1;
+    }
+    return 0;
+  });
+}
+
+function lastIfNotFound(index: number): number {
+  return index >= 0 ? index : Number.POSITIVE_INFINITY;
 }
