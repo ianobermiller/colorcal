6e36426b109b70dbc8a81a13af4853bc2b3889dc
Ian Obermiller <ian@obermillers.com>
2024-04-21 21:04:07 -0400

    Remove landing css

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -1,6 +1,5 @@
 import { CalendarGrid } from './CalendarGrid';
 import { exampleData } from './exampleCalendarData';
-import styles from './Landing.module.css';
 
 interface Props {
   /** for preact-router */
@@ -9,13 +8,13 @@ interface Props {
 
 export function Landing(_: Props) {
   return (
-    <div class={styles.root}>
+    <div class="max-w-2xl">
       <p>
         Organizing a multi-city trip is hard, keeping track of where you are going and for how long. Use Color Calendar
         to paint on the days, see at a glance how long you are staying in each location, and have a colorful big picture
         of your entire trip.
       </p>
-      <div class={styles.exampleCalendar}>
+      <div class="mt-4 inline-block rounded bg-slate-100 p-6">
         <h2>Example Trip to the UK</h2>
         <CalendarGrid {...exampleData} />
       </div>
