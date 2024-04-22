9a4f66fc46832992bc0ab5a41cc67ac8a103dae9
Ian Obermiller <ian@obermillers.com>
2024-04-21 21:55:46 -0400

    Remove button css

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,22 +1,33 @@
 import clsx from 'clsx';
-import styles from './Button.module.css';
 
 type Props = JSX.HTMLAttributes<HTMLButtonElement>;
 
+const buttonClasses = clsx(
+  'flex h-8 cursor-pointer items-center gap-2 rounded-lg border-2 border-black px-3 hover:bg-slate-100',
+);
+
 export function Button({ class: className, ...otherProps }: Props) {
-  return <button {...otherProps} class={clsx(styles.button, className)} />;
+  return <button {...otherProps} class={clsx(buttonClasses, className)} />;
 }
 
 export function ButtonLink({ class: className, ...otherProps }: JSX.HTMLAttributes<HTMLAnchorElement>) {
   // This is a pass-through component
   // eslint-disable-next-line jsx-a11y/anchor-has-content
-  return <a {...otherProps} class={clsx(styles.button, className)} />;
+  return <a {...otherProps} class={clsx(buttonClasses, className)} />;
 }
 
 export function IconButton({ class: className, ...otherProps }: Props) {
-  return <button {...otherProps} class={clsx(styles.iconButton, className)} />;
+  return (
+    <button
+      {...otherProps}
+      class={clsx(
+        'inline-flex size-8 cursor-pointer items-center justify-center rounded-lg hover:bg-slate-100',
+        className,
+      )}
+    />
+  );
 }
 
 export function LinkButton({ class: className, ...otherProps }: Props) {
-  return <button {...otherProps} class={clsx(styles.linkButton, className)} />;
+  return <button {...otherProps} class={clsx('cursor-pointer underline', className)} />;
 }
