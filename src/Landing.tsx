a0c7ed8ad6061ee2c0f73a5dcc4327b542b5c23a
Ian Obermiller <ian@obermillers.com>
2025-05-16 07:23:09 -0400

    Fix landing page

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -14,7 +14,7 @@ export function Landing(_: Props) {
         to paint on the days, see at a glance how long you are staying in each location, and have a colorful big picture
         of your entire trip.
       </p>
-      <div className="mt-4 inline-block rounded bg-slate-100 p-6">
+      <div className="mt-4 rounded bg-slate-100 p-6 dark:bg-slate-800">
         <h2>Example Trip to the UK</h2>
         <CalendarGrid {...exampleData} />
       </div>
