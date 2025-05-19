3411489b99dfe9c9b4c771b878ad6397ebc6e36f
Ian Obermiller <ian@obermillers.com>
2025-05-19 09:47:56 -0400

    Add bundle analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import preact from '@preact/preset-vite';
 // @ts-expect-error module not found
 import tailwindcss from '@tailwindcss/vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
@@ -9,5 +10,6 @@ export default defineConfig({
     preact(),
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     tailwindcss(),
+    visualizer({ gzipSize: true }),
   ],
 });
