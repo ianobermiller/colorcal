fd3a48148e007d659a99eafb1d35b469ea99ebac
Ian Obermiller <ian@obermillers.com>
2025-05-26 10:43:33 -0400

    Move Portal into Modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,7 @@
 import type { JSX } from 'solid-js';
 
 import XIcon from '~icons/feather/x';
+import { Portal } from 'solid-js/web';
 
 import { IconButton } from './Button';
 
@@ -20,16 +21,21 @@ export function Modal(props: Props) {
     };
 
     return (
-        <div class="fixed inset-0 flex items-start justify-center bg-black/50 px-4 pt-12" onClick={handleScrimClick}>
-            <div class="flex w-full flex-col gap-4 rounded border-slate-300 bg-white px-6 py-2 shadow-lg md:max-w-lg dark:bg-slate-800">
-                <header class="flex items-center justify-between border-b border-slate-400 py-3">
-                    <h2 class="text-lg font-bold">{props.title}</h2>
-                    <IconButton onClick={props.onClose}>
-                        <XIcon height="16" width="16" />
-                    </IconButton>
-                </header>
-                {props.children}
+        <Portal>
+            <div
+                class="fixed inset-0 flex items-start justify-center bg-black/50 px-4 pt-12"
+                onClick={handleScrimClick}
+            >
+                <div class="flex w-full flex-col gap-4 rounded border-slate-300 bg-white px-6 py-2 shadow-lg md:max-w-lg dark:bg-slate-800">
+                    <header class="flex items-center justify-between border-b border-slate-400 py-3">
+                        <h2 class="text-lg font-bold">{props.title}</h2>
+                        <IconButton onClick={props.onClose}>
+                            <XIcon height="16" width="16" />
+                        </IconButton>
+                    </header>
+                    {props.children}
+                </div>
             </div>
-        </div>
+        </Portal>
     );
 }
